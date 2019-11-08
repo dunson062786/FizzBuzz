09fc263ceeae02a33c8f3c500a1272069da1ac87
Matt Dunson <dunson.142000@gmail.com>
2019-11-08 10:51:06 -0500

    Added more unit tests

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -3,7 +3,10 @@ defmodule FizzBuzz do
   def fizzbuzz(number) when is_integer(number) and rem(number, 3) == 0, do: "fizz"
   def fizzbuzz(number) when is_integer(number) and rem(number, 5) == 0, do: "buzz"
 
-  def fizzbuzz(number) do
+  def fizzbuzz(number) when is_integer(number) do
     number
+  end
+
+  def fizzbuzz(list) when is_list(list) do
   end
 end
--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -2,19 +2,39 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
   doctest FizzBuzz
 
-  test "returns fizz if multiple of 3" do
+  test "returns number since 1 is not multiple of 3 or 5" do
+    assert FizzBuzz.fizzbuzz(1) == 1
+  end
+
+  test "returns number since 2 is not multiple of 3 or 5" do
+    assert FizzBuzz.fizzbuzz(2) == 2
+  end
+
+  test "returns fizz since 3 is a multiple of 3" do
     assert FizzBuzz.fizzbuzz(3) == "fizz"
   end
 
-  test "returns buzz if multiple of 5" do
+  test "returns fizz since 6 is a multiple of 3" do
+    assert FizzBuzz.fizzbuzz(6) == "fizz"
+  end
+
+  test "returns buzz since 5 is a multiple of 5" do
     assert FizzBuzz.fizzbuzz(5) == "buzz"
   end
 
-  test "return fizzbuzz if multiple of 3 and 5" do
+  test "returns buzz since 10 is a multiple of 5" do
+    assert FizzBuzz.fizzbuzz(10) == "buzz"
+  end
+
+  test "return fizzbuzz since 15 is a multiple of 3 and 5" do
     assert FizzBuzz.fizzbuzz(15) == "fizzbuzz"
   end
 
-  test "prints number if not multiple of 3 or 5" do
+  test "return fizzbuzz since 30 is a multiple of 3 and 5" do
+    assert FizzBuzz.fizzbuzz(30) == "fizzbuzz"
+  end
+
+  test "returns number since 8 is not a multiple of 3 or 5" do
     assert FizzBuzz.fizzbuzz(8) == 8
   end
 end
